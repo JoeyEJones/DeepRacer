4af21cf19212ff7a4008925ee1c51a07a3819e69
JoeyEJones <JoeyEJones@users.noreply.github.com>
2019-08-06 07:01:14 -0700

    Update DeepRacer1.py
    Added a lot of additional incentives.

--- DeepRacer1.py
+++ DeepRacer1.py
@@ -16,44 +16,36 @@ def reward_function(params):
     progress = params['progress']
     maxSteeringAngle = 22.0
     maxSpeed = 8.0
-    reward = 100 # This is what we get each step just for being on the track still. It should prevent us from driving really fast right off course and calling that a high score.
     
-    '''
-    Set the reward based upon the car's speed relative to its steering angle.
-    '''
-    reward = reward + ((maxSpeed - (speed * ((steeringAbs + 1) / maxSteeringAngle))) * speed + (speed / 2))
+    # Our AWS re:Invent 2018 target is currently consistent 10 second consistent laps.
+    desiredSeconds = 10
+    stepsPerSecond = 15
     
-    '''
-    Models that made it further in the track are significantly more desirable
-    '''
+    # This is the calculated desired maximum steps before we are going way too slow.
+    maxStepsBeforeTooSlow = desiredSeconds * stepsPerSecond
     
+    # This is what we get each step just for being on the track still. It should prevent us from driving really fast right off course and calling that a high score.
+    reward = 100 
+    
+    # Set the reward based upon the car's speed relative to its steering angle.
+    reward = reward + ((maxSpeed - (speed * ((steeringAbs + 1) / maxSteeringAngle))) * speed)
+    
+    # Models that made it further in the track are significantly more desirable
     #reward = reward * (progress / 100)
     
-    '''
-    Reduce the reward ever so slightly if the car is completely on the track (we want to cut corners).
-    '''
-    #if on_track:
-    #    reward = reward * 0.98
+    # Incentivize speed straight up.
+    #reward = reward + speed
     
     '''
-    Reduce the reward if the car is left of center and turning left or right of center and turning right.
-
-    if leftOfCenter and steeringAngle > 3:
-        reward = reward * 0.90
-    elif (not leftOfCenter) and steeringAngle < -3:
-        reward = reward * 0.90
-    elif leftOfCenter and steeringAngle < -3:
-        reward = reward * 1.10
-    elif (not leftOfCenter) and steeringAngle > 3:
-        reward = reward * 1.10
-    else:
-        reward = reward
+    If we're dipping one, two, or three wheels off the track we can ever so slightly reward that to incentivize cutting corners. This, combined with a minimum steps algorithm should slowly instruct the car to find a true racing line.
     '''
+    if not on_track:
+        reward = reward * 1.01
     
     # Calculate 3 markers that are at varying distances away from the center line
-    marker_near = 0.1 * track_width
-    marker_med = 0.25 * track_width
-    marker_far = 0.5 * track_width
+    marker_near = 0.15 * track_width
+    marker_med = 0.30 * track_width
+    marker_far = 0.50 * track_width
     
     if leftOfCenter:
         # If we're close to the center we should reward low steering angles.
@@ -112,5 +104,9 @@ def reward_function(params):
         # If we're not full opposite lock we're probably done.
         elif steeringAngle != 22:
             reward = 1e-3
-            
+    '''
+    We want to incentivize the program to use as few steps as possible so it should start getting negative points if it gets over 225 steps (roughly 15 seconds of course time). By adding in the progress multiplier the program will trend towards a middle ground where it tries to follow the track while making progress as quickly as possible. 
+    '''
+    reward = reward + ((1 - (steps / maxStepsBeforeTooSlow)) * progress)
+    
     return float(reward)
